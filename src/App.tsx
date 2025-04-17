021a65b37482463aa3865e31de2f80a611fc1e09
jesustissera <jesustissera@gmail.com>
2025-04-17 15:05:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Content from "./components/Content/Content.tsx";
+
+function App() {
+  
+  return (
+    <>
+    <Content />
+    </>    
+  );
+}
+
+export default App;
